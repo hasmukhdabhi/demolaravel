0a2011dd3e0d50f2f07cb4c60651edfb28cad719
hasmukhdabhi <skey.hasmukh@gmail.com>
2025-01-30 17:12:33 +0530

    livewire style and scrpits

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Counter</title>
+    @livewireStyles
 </head>
 <body>
     <div>
@@ -13,5 +14,6 @@
         <button wire:click="increment">++</button>
         <button wire:click="decrement">--</button>
     </div>
+    @livewireScripts
 </body>
 </html>
